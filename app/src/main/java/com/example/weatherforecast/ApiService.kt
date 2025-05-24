a707e7e8998d18602fa8616b5f20dbcdd26c80cc
YoussefMostafaMohammed <youssefmostafa453@gmail.com>
2025-05-24 11:33:25 +0300

    create retrofit client and get the data

--- app/src/main/java/com/example/weatherforecast/ApiService.kt
+++ app/src/main/java/com/example/weatherforecast/ApiService.kt
@@ -1,4 +1,21 @@
 package com.example.weatherforecast
 
-class ApiService {
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("forecast")
+    suspend fun getWeather(
+        @Query("id") cityId: Int,
+        @Query("appid") apiKey: String,
+        @Query("units") units: String = "metric"
+    ): Response<WeatherResponse>
+
+    @GET("weather")
+    suspend fun getCurrentWeather(
+        @Query("q") cityName: String,
+        @Query("appid") apiKey: String,
+        @Query("units") units: String = "metric"
+    ): Response<CurrentWeatherResponse>
 }
