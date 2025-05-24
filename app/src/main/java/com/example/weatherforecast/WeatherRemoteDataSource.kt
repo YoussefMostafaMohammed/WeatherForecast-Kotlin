7972ebd6622716385e560bbf39623d0f35812e71
YoussefMostafaMohammed <youssefmostafa453@gmail.com>
2025-05-24 11:36:36 +0300

    Create Remote Data Source

--- app/src/main/java/com/example/weatherforecast/WeatherRemoteDataSource.kt
+++ app/src/main/java/com/example/weatherforecast/WeatherRemoteDataSource.kt
@@ -1,4 +1,9 @@
 package com.example.weatherforecast
 
+import com.example.weatherforecast.CurrentWeatherResponse
+import com.example.weatherforecast.WeatherResponse
+
 interface WeatherRemoteDataSource {
+    suspend fun fetchCurrentWeather(cityName: String, apiKey: String, units: String = "metric"): CurrentWeatherResponse
+    suspend fun fetchForecast(cityId: Int, apiKey: String, units: String = "metric"): WeatherResponse
 }
--- app/src/main/java/com/example/weatherforecast/WeatherRemoteDataSourceImpl.kt
+++ app/src/main/java/com/example/weatherforecast/WeatherRemoteDataSourceImpl.kt
@@ -1,4 +1,28 @@
 package com.example.weatherforecast
 
-class WeatherRemoteDataSourceImpl {
+import com.example.weatherforecast.ApiService
+
+class WeatherRemoteDataSourceImpl(
+    private val api: ApiService
+) : WeatherRemoteDataSource {
+
+    override suspend fun fetchCurrentWeather(
+        cityName: String,
+        apiKey: String,
+        units: String
+    ): CurrentWeatherResponse {
+        val response = api.getCurrentWeather(cityName, apiKey, units)
+        if (response.isSuccessful) return response.body()!!
+        throw Exception("Error fetching current: ${'$'}{response.code()} ${'$'}{response.message()}")
+    }
+
+    override suspend fun fetchForecast(
+        cityId: Int,
+        apiKey: String,
+        units: String
+    ): WeatherResponse {
+        val response = api.getWeather(cityId, apiKey, units)
+        if (response.isSuccessful) return response.body()!!
+        throw Exception("Error fetching forecast: ${'$'}{response.code()} ${'$'}{response.message()}")
+    }
 }
