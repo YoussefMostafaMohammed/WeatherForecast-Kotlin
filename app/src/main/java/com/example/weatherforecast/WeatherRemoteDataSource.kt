4080558c31536686712359d0730647aa95af9007
YoussefMostafaMohammed <youssefmostafa453@gmail.com>
2025-05-25 17:33:32 +0300

    Handle Favorite Fragment

--- app/src/main/java/com/example/weatherforecast/WeatherRemoteDataSource.kt
+++
@@ -1,9 +0,0 @@
-package com.example.weatherforecast
-
-import com.example.weatherforecast.CurrentWeatherResponse
-import com.example.weatherforecast.WeatherResponse
-
-interface WeatherRemoteDataSource {
-    suspend fun fetchCurrentWeather(cityName: String, apiKey: String, units: String = "metric"): CurrentWeatherResponse
-    suspend fun fetchForecast(cityId: Int, apiKey: String, units: String = "metric"): WeatherResponse
-}
